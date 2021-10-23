8d5443fb243e33995c238e33d82366bcfff4ed0a
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2021-10-23 00:14:59 -0500

    Blank Window

---
+++ gui.py
@@ -0,0 +1,12 @@
+import tkinter
+
+#Initial Stuff
+name = "Python Expert System"
+window = tkinter.Tk()
+window.title(name)
+window.geometry("500x350")
+window.minsize(width=500,height=250)
+
+
+
+window.mainloop()
