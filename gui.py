5904e2dc551ef6e59a0106b4cd350a6178109be0
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2021-10-23 02:32:39 -0500

    added code to make blood pressure prompts appear under condition

--- gui.py
+++ gui.py
@@ -25,13 +25,35 @@ lbl_dizzy = tkinter.Label(window,text="Dizziness")
 lbl_faint = tkinter.Label(window,text="Fainting")
 lbl_BV = tkinter.Label(window,text="Blurred Vision")
 
+def displayBP():
+    if(isDizzy.get()==1 or isBV.get()==1 or isFaint.get()==1):
+        lbl_dbv.grid(row=5,column=0)
+        ent_dbv.grid(row=5,column=1)
+        lbl_sbv.grid(row=6,column=0)
+        ent_sbv.grid(row=6,column=1)
+        print("This works!")
+    else:
+        print("tedst")
+        lbl_dbv.grid_forget()
+        ent_dbv.grid_forget()
+        lbl_sbv.grid_forget()
+        ent_sbv.grid_forget()
+
+
+
+
 #Checkboxes for the last three
-chb_dizzy = tkinter.Checkbutton(window, text="Dizzy",variable=isDizzy,onvalue=1, offvalue=0)
-chb_faint= tkinter.Checkbutton(window, text="Faint",variable=isFaint,onvalue=1, offvalue=0)
-chb_bv = tkinter.Checkbutton(window, text="Blurred Vision",variable=isBV,onvalue=1, offvalue=0)
+chb_dizzy = tkinter.Checkbutton(window, text="",variable=isDizzy,onvalue=1, offvalue=0,command=displayBP)
+chb_faint= tkinter.Checkbutton(window, text="",variable=isFaint,onvalue=1, offvalue=0,command=displayBP)
+chb_bv = tkinter.Checkbutton(window, text="",variable=isBV,onvalue=1, offvalue=0, command=displayBP)
 
+
+btn_submit = tkinter.Button(window,text="Analyze!")
 #For Blood Pressure, ONLY ask if patient is dizzy, faint or BV
-
+lbl_dbv = tkinter.Label(window,text="Blood Pressure (diastolic)")
+ent_dbv = tkinter.Entry(window,width=3)
+lbl_sbv = tkinter.Label(window,text="Blood Pressure (systolic)")
+ent_sbv = tkinter.Entry(window,width=3)
 
 
 
@@ -40,12 +62,19 @@ chb_bv = tkinter.Checkbutton(window, text="Blurred Vision",variable=isBV,onvalue
 #Grid Section
 lbl_Temp.grid(row=0,column=0)
 ent_Tvalue.grid(row=0,column=1)
-#lbl_dizzy.grid(row=1,column=0)
-chb_dizzy.grid(row=1,column=0)
-chb_faint.grid(row=2,column=0)
-chb_bv.grid(row=3,column=0)
-#lbl_faint.grid(row=2,column=0)
-#lbl_BV.grid(row=3,column=0)
+
+lbl_dizzy.grid(row=1,column=0)
+chb_dizzy.grid(row=1,column=1)
+
+lbl_faint.grid(row=2,column=0)
+chb_faint.grid(row=2,column=1)
+
+lbl_BV.grid(row=3,column=0)
+chb_bv.grid(row=3,column=1)
+
+btn_submit.grid(row=4,column=0)
 
 
+
+window.bind()
 window.mainloop()
