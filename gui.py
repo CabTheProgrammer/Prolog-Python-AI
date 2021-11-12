719c4e4ec09e031fbbf389a9c998976a05df71c3
CabTheProgrammer;echo <55765253+CabTheProgrammer@users.noreply.github.com>
2021-11-12 03:18:14 -0500

    Updated the gui

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
 import tkinter     # this is a gui library for python
+import patient
 from tkinter import *
 
+
 #Initial Stuff
-name = "Python Expert System"
+name = "Python Expert System(PES)"
 window = tkinter.Tk()
 window.title(name)
 window.geometry("600x500")# Sets window size
@@ -13,16 +15,22 @@ basicinfo = Frame(window,borderwidth=2,relief=RIDGE)
 basicinfo.grid(row=0,column=0)
 symptoms = Frame(window,borderwidth=2,relief=RIDGE)
 symptoms.grid(row=0,column=1,padx=5)
+display = Frame(window,borderwidth=2,relief=RIDGE)
+display.grid(row=2,columnspan = 2)
+
 
 #boolean values for symptons
 isDizzy = IntVar()
 isFaint = IntVar()
 isBV = IntVar()
 sex=StringVar(value='male')
-vax = IntVar() #1 if the person is vaccinated; 0 if they are not
+vax = StringVar(value='Unvaccinated') #1 if the person is vaccinated; 0 if they are not
 isNausea = IntVar()
 isshortofbreath = IntVar()
 iseasilytired = IntVar()
+
+#string variables
+
 #TODO: 
 #Sample run is to go in System Design.
 #Analyze should display likelihood patient has covid and add them to the knowledge base.
@@ -58,8 +66,8 @@ rad_male = tkinter.Radiobutton(basicinfo, text="Male",variable=sex,value='male')
 rad_female = tkinter.Radiobutton(basicinfo, text="Female",variable=sex,value='female')
 #Vaccination Status
 lbl_vax = tkinter.Label(basicinfo,pady=10,text="Vaccination Status")
-rad_vax = tkinter.Radiobutton(basicinfo, text="Vaccinated",variable=vax,value=1)
-rad_novax = tkinter.Radiobutton(basicinfo, text="Unvaccinated",variable=vax,value=0)
+rad_vax = tkinter.Radiobutton(basicinfo, text="Vaccinated",variable=vax,value="Vaccinated")
+rad_novax = tkinter.Radiobutton(basicinfo, text="Unvaccinated",variable=vax,value="Unvaccinated")
 
 
 lbl_Temp = tkinter.Label(basicinfo,text="Temperature(C)")
@@ -69,6 +77,24 @@ ent_Tvalue = tkinter.Entry(basicinfo,width=3)
 lbl_dizzy = tkinter.Label(symptoms,text="Dizziness")
 lbl_faint = tkinter.Label(symptoms,text="Fainting")
 lbl_BV = tkinter.Label(symptoms,text="Blurred Vision")
+
+
+#for output
+txt_display = tkinter.Text(display,height=18,width=70)
+txt_display.insert('1.35','===Analysis Output===')
+txt_display['state']= 'disabled' #prevents user from editing text
+# txt_display['state'] = 'normal' how to display text
+txt_display.grid(row =0,column = 0)
+
+def DisplayString(astring): # Function to display stuff in the text box
+    txt_display['state'] = 'normal'
+    txt_display.delete('1.0', END) #clears display  
+    txt_display.insert('1.35','===Analysis Output===')
+    txt_display.insert('2.0',astring)
+    txt_display['state']= 'disabled'
+
+
+    pass
 
 def displayBP(): #Displays options to capture blood pressure should the user click any of the options
     if(isDizzy.get()==1 or isBV.get()==1 or isFaint.get()==1):
@@ -83,10 +109,41 @@ def displayBP(): #Displays options to capture blood pressure should the user cli
         ent_sbv.grid_forget()
 
 def gatherinput():
-    name = ent_name.get("1.0",END) # Captures text from input
-    age = ent_age.get("1.0","end-1c")
-    temp = ent_Tvalue.get("1.0",END)
-    # TODO: Capture other values!
+    name =ent_name.get() #pname #ent_name.get("1.0","end-1c") # Captures text from input
+    age = ent_age.get()
+    temp = ent_Tvalue.get()
+    symptoms = set()
+
+    diastolic = 0
+    systolic = 0
+    
+    if(isDizzy.get()==1 or isBV.get()==1 or isFaint.get()==1):
+         diastolic = ent_dbv.get()
+         systolic = ent_sbv.get()
+
+
+    if(isDizzy.get() == 1):
+        symptoms.add("Dizzy")
+    if(isFaint.get() == 1):
+        symptoms.add("Faint")
+    if(isBV.get() == 1):
+        symptoms.add("Blurred Vision")
+    if(isNausea.get() == 1):
+        symptoms.add("Nausea")
+    if(isBV.get() == 1):
+        symptoms.add("Blurred Vision")
+    if(isshortofbreath.get() == 1):
+        symptoms.add("Short of Breath")
+    if(iseasilytired.get() == 1):
+        symptoms.add("Easily Tired") 
+        
+
+    p1 = patient.Patient(name,age,sex.get(),temp,vax.get(),symptoms,systolic,diastolic)
+    p1.pinfo()
+    DisplayString(p1.pstring())
+    
+ 
+
 
 
 #Checkboxes for the rest
@@ -103,7 +160,7 @@ lbl_shortbreath = tkinter.Label(symptoms,text="Short of Breath")
 lbl_easily = tkinter.Label(symptoms,text="Easily tired")
 
 
-btn_submit = tkinter.Button(symptoms,text="Analyze!") #Adds patient's data to file and displays analysis results
+btn_submit = tkinter.Button(symptoms,text="Analyze!",command = gatherinput) #Adds patient's data to file and displays analysis results
 btn_statistics = tkinter.Button(symptoms,text="Statistics!") # Displays overall statistics on screen
 #For Blood Pressure, ONLY ask if patient is dizzy, faint or BV
 lbl_dbv = tkinter.Label(symptoms,text="Blood Pressure (diastolic)")
